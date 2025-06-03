752546aea935397e1cae0c8ab5ccd79ae213a9e5
Subba <kvsubbareddy238@gmail.com>
2025-06-03 06:52:52 +0530

    registration pages

--- src/test/java/Pages/Hooks.java
+++ src/test/java/Pages/Hooks.java
@@ -14,16 +14,17 @@ import java.io.File;
 import java.io.IOException;
 
 public class Hooks {
- WebDriver driver= Utility.getDefaultDriver();
-   @Before
+    WebDriver driver = Utility.getDefaultDriver();
+
+    @Before
     public void setUp() {
         // This method will run before each scenario
         System.out.println("Setting up the test environment...");
         // You can add more setup code here, like initializing WebDriver, etc.
 
-      //  driver.get("http://www.automationpractice.pl/index.php");
+        //  driver.get("http://www.automationpractice.pl/index.php");
 
-       driver.get("https://demo.prestashop.com/#/en/front");
+        driver.get("https://demo.prestashop.com/#/en/front");
     }
 
     @After
@@ -33,18 +34,23 @@ public class Hooks {
         // You can add more teardown code here, like closing WebDriver, etc.
         WebDriver driver = Utility.getDefaultDriver();
         if (driver != null) {// Quit WebDriver after the test
-driver.quit();
+            driver.quit();
             Utility.resetDriver(); // Reset the driver to avoid reuse
         }
     }
 
+    @AfterStep
     public void addScreenShot(Scenario scenario) throws IOException {
         if (scenario.isFailed()) {
-            System.out.println("The scenario has failed. Taking a screenshot...");
-            TakesScreenshot scrht = (TakesScreenshot) driver;
-            File sourcePath = scrht.getScreenshotAs(OutputType.FILE);
-            byte[] fileContent = FileUtils.readFileToByteArray(sourcePath);
-            scenario.attach(fileContent, "image/png", "image");
+            try {
+                System.out.println("The scenario has failed. Taking a screenshot...");
+                TakesScreenshot scrht = (TakesScreenshot) driver;
+                byte[] fileContent = scrht.getScreenshotAs(OutputType.BYTES);
+                scenario.attach(fileContent, "image/png", "Failure Screenshot");
+            } catch (Exception e) {
+                System.err.println("Failed to capture screenshot: " + e.getMessage());
+                e.printStackTrace();
+            }
         }
     }
 }
