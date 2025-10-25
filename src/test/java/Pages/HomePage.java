13ff0f80b68c8fab0b047ab786a0595ba9f9459a
Subba <kvsubbareddy238@gmail.com>
2025-10-25 12:25:44 +0530

    Parallel execution docker file selenium grid added

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -12,26 +12,40 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.Utility;
 
 import java.time.Duration;
+import java.util.List;
 
 
 public class HomePage {
     @FindBy(how = How.XPATH, using = "//a[normalize-space()='Sign in']")
     public WebElement signInButton;
 
-   WebDriver driver ;
-    public  HomePage(WebDriver driver)
-    {
-        this.driver= driver;
+    @FindBy(how = How.XPATH, using = "//div[contains(@class,'title') and contains(@data-toggle,'collapse')]")
+    public List<WebElement> sectionTitles;
+
+    @FindBy(how = How.XPATH, using = ".//span[@class='h3']")
+    public WebElement linksUnderFooterHeaders;
+
+
+    WebDriver driver;
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
         PageFactory.initElements(driver, this);
 
     }
 
-    public void clickOnSignIn(){
+    public void clickOnSignIn() {
 
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement signInButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='Sign in']")));
-            signInButton.click();
-            String title = driver.getTitle();
-            System.out.println("Page title is: " + title);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement signInButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='Sign in']")));
+        signInButton.click();
+        String title = driver.getTitle();
+        System.out.println("Page title is: " + title);
+    }
+
+    public void getFooterHeaders() throws InterruptedException {
+
+
     }
 }
